2df55f3bf77f27e8810f08a8613e678b5b570911
hannan <abdul.hannan1@arbisoft.com>
2023-08-30 13:45:38 +0500

    Tests

--- djangochat 2/core/tests.py
+++ djangochat 2/core/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import  TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CoreViewsTest(TestCase):
+     # Test the frontpage view
+    def test_frontpage_view(self):
+        # Send a GET request to the 'frontpage' URL
+        response = self.client.get(reverse('frontpage'))
+        self.assertEqual(response.status_code, 200)
+        # Check if the response contains the text "Djangochat"
+        self.assertContains(response, "Djangochat")
+        
+    # Test the individual_chats view
+    def test_individual_chats_view(self):
+        response = self.client.get(reverse('individual_chats'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Individual Chats")
+        
+    # Test the user_list view
+    def test_user_list_view(self):
+        # Log in with a test user
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('user_list'))
+        self.assertEqual(response.status_code, 200)
+        # Check if the response contains the text "User List"
+        self.assertContains(response, "User List")
--- djangochat 2/room/tests.py
+++ djangochat 2/room/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+# Test Case for joining Individual Chat
+from asgiref.testing import ApplicationCommunicator
+from room.consumers import IndivConsumer
+from django.test import Client, TestCase
+from django.urls import reverse
+from room.models import Message
+from django.contrib.auth.models import User
+import pytest
 
-# Create your tests here.
+from room.models import IndiviualChat, Room
+
+@pytest.mark.asyncio
+async def test_join_individual_chat():
+    communicator = ApplicationCommunicator(IndivConsumer, {'type': 'websocket.connect', 'path': '/ws/testuser/'})
+    connected, _ = await communicator.connect()
+
+    assert connected
+
+    await communicator.disconnect()
+    
+
+class RoomViewsTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.another_user = User.objects.create_user(username='anotheruser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.room = Room.objects.create(name='Test Room', slug='test-room')
+        self.message = Message.objects.create(room=self.room, user=self.user, content='Hello, World!')
+
+    def test_user_list_view(self):
+        response = self.client.get(reverse('user_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "User List")
