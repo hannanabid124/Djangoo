5c34086a16f258bb7d226add8996cfbf70d9ff06
hannan <abdul.hannan1@arbisoft.com>
2023-09-01 16:16:42 +0500

    postgresql

--- settings.py
+++ settings.py
@@ -95,13 +95,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo',
+        'USER': 'postgres',
+        'PASSWORD': 'arbisoft',
+        'POST': '5434',
+        'HOST': 'localhost'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
