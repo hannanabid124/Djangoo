7714b9086b334bd55e74387f5f7a9988c9c8ebde
hannan <abdul.hannan1@arbisoft.com>
2023-09-12 22:35:22 +0500

    Postgresql

---
+++ djangochat 2(Done)/setting.py
@@ -0,0 +1,26 @@
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+'''DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+}'''
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo',
+        'USER': 'postgres',
+        'PASSWORD': 'arbisoft',
+        'POST': '5434',
+        'HOST': 'localhost'
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
