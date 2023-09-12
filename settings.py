4de5a32cf1ed59542286a57efeaec7cb58548627
Abdul Hannan Abid <139880308+hannanabid124@users.noreply.github.com>
2023-09-12 21:47:06 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -95,14 +95,6 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}'''
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
