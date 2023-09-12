e8d9adb838bec5faed5b15427172c5c11089b1ef
hannan <abdul.hannan1@arbisoft.com>
2023-09-12 22:28:59 +0500

    Changes

--- djangochat 2(Done)/room/admin.py
+++ djangochat 2(Done)/room/admin.py
@@ -5,8 +5,3 @@ from .models import Room
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     pass
-'''from django.contrib import admin
-
-from .models import Room
-
-admin.site.register(Room)'''
